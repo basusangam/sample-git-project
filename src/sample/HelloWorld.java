6cbe5caff7845327559a3775981f4696f1f685bf
basusangam <basavaraj.sangam@gmail.com>
2017-02-04 08:08:55 +0530

    intial commit

---
+++ src/sample/HelloWorld.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+
+        System.out.println("Hello, World...ABC Manjunath kumatagi");
+        System.out.println("git ");
+    }
+
+}
